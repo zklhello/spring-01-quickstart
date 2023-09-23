141b61874c833eee80f02e1bf90a381fef11fafb
zklhello <zklhello@163.com>
2023-09-23 12:11:38 +0800

    第一次提交

---
+++ src/main/java/com/itheima/App2.java
@@ -0,0 +1,32 @@
+package com.itheima;
+
+import com.itheima.dao.BookDao;
+import com.itheima.service.BookService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.awt.print.Book;
+
+/**
+ * ClassName: App2
+ * Package: com.itheima
+ * Description:
+ *
+ * @Author 周康来
+ * @Create 2023/9/19 21:09
+ */
+public class App2 {
+
+    public static void main(String[] args) {
+        ApplicationContext ctx =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+/*        BookDao bookDao = (BookDao) ctx.getBean("bookDao");
+        bookDao.save();
+
+        System.out.println("|-----------------------------|");*/
+
+        BookService bookService = (BookService) ctx.getBean("bookService");
+        bookService.save();
+
+    }
+}
