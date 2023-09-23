5d869ab4b8a2f11b3529a14c7090be384fc4d12b
zklhello <zklhello@163.com>
2023-09-23 12:20:19 +0800

    对App2.java文件的修改

--- src/main/java/com/itheima/App2.java
+++ src/main/java/com/itheima/App2.java
@@ -20,10 +20,10 @@ public class App2 {
     public static void main(String[] args) {
         ApplicationContext ctx =
                 new ClassPathXmlApplicationContext("applicationContext.xml");
-/*        BookDao bookDao = (BookDao) ctx.getBean("bookDao");
+        BookDao bookDao = (BookDao) ctx.getBean("bookDao");
         bookDao.save();
 
-        System.out.println("|-----------------------------|");*/
+        System.out.println("|-----------------------------|");
 
         BookService bookService = (BookService) ctx.getBean("bookService");
         bookService.save();
